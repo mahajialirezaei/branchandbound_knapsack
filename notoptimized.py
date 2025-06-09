bc7d11e3405c719d9ea4766e4e261ca2e28a543b
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-09 21:14:49 +0330

    def recursive_func

--- notoptimized.py
+++ notoptimized.py
@@ -2,3 +2,23 @@ def cal_product_ls(x: list, p: list):
     profit = sum(s * s1 for (s, s1) in zip(x, p))
     return profit
 
+
+def recursive_func(x: list, p: list, w: list, l, b, cur_w):
+    global x_opt, p_opt
+    print(x_opt)
+    print(p_opt)
+    if l == len(x):
+        p_cur = cal_product_ls(x, p)
+        if p_cur > p_opt:
+            p_opt = p_cur
+            x_opt = x.copy()
+    else:
+        c = [0]
+        if cur_w + w[l] <= b:
+            c = [0, 1]
+        for xl in c:
+            x[l] = xl
+            recursive_func(x, p, w, l + 1, b, cur_w + w[l] * xl)
+
+
+
